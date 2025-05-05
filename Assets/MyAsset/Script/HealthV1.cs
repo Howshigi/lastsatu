6729e7d04fe047e672878eed38fffaa3bc592062
Howshigi <thitigorn.putt@bumail.net>
2025-05-05 22:26:51 +0700

    Score

--- Assets/MyAsset/Script/HealthV1.cs
+++ Assets/MyAsset/Script/HealthV1.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class HealthV1 : MonoBehaviour
 {
     public int health = 100;
+    public int scoreValue = 10;
 
     public void TakeDamage(int damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            if (ScoreManager.Instance != null)
+            {
+                ScoreManager.Instance.AddScore(scoreValue);
+            }
             Destroy(gameObject);
         }
     }
